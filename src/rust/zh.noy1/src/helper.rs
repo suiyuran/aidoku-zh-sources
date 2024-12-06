d9d901dacbd82bac26dd6c01cd88fa748b09859c
suiyuran <suiyuran@163.com>
2024-12-06 11:36:52 +0800

    fix zh.noy1

--- src/rust/zh.noy1/src/helper.rs
+++ src/rust/zh.noy1/src/helper.rs
@@ -1,12 +1,13 @@
 use aidoku::{
 	error::{AidokuError, AidokuErrorKind},
-	prelude::*,
+	helpers::substring::Substring,
 	std::{
-		defaults::defaults_get,
+		defaults::{defaults_get, defaults_set},
 		net::{HttpMethod, Request},
-		String, ValueRef,
+		String, StringRef, ValueRef,
 	},
 };
+use alloc::{format, string::ToString};
 
 pub const WWW_URL: &str = "https://noy1.top";
 const PIC_URL: &str = "https://img.noy.asia";
@@ -51,21 +52,65 @@ pub fn details(manga_id: String) -> Result<ValueRef, AidokuError> {
 	get_json(url, body)
 }
 
-pub fn get_json(url: String, body: String) -> Result<ValueRef, AidokuError> {
-	let session = defaults_get("session")?.as_string()?.read();
+pub fn gen_request(url: String, method: HttpMethod) -> Request {
+	let session = defaults_get("session").unwrap().as_string().unwrap().read();
+	let session = if !url.contains("login") && session.is_empty() {
+		login().unwrap()
+	} else {
+		session
+	};
+	Request::new(url, method)
+		.header("Content-Type", "application/x-www-form-urlencoded")
+		.header("Cookie", &format!("NOY_SESSION={}", session))
+}
 
-	if session.is_empty() {
+pub fn login() -> Result<String, AidokuError> {
+	let url = format!("{}/api/login", WWW_URL);
+	let request = gen_request(url, HttpMethod::Post);
+	let username = defaults_get("username")?.as_string()?.read();
+	let password = defaults_get("password")?.as_string()?.read();
+
+	if username.is_empty() || password.is_empty() {
 		return Err(AidokuError {
 			reason: AidokuErrorKind::DefaultNotFound,
 		});
 	}
 
-	let request = Request::new(url, HttpMethod::Post)
-		.header("Cookie", &format!("NOY_SESSION={}", session))
-		.header("Content-Type", "application/x-www-form-urlencoded")
-		.body(body.as_bytes());
+	let body = format!("user={}&pass={}", username, password);
+	let request = request.body(body.as_bytes());
 
-	request.json()
+	request.send();
+
+	if request.status_code() != 200 {
+		return Err(AidokuError {
+			reason: AidokuErrorKind::DefaultNotFound,
+		});
+	}
+
+	let cookie_header = request.get_header("set-cookie").unwrap().read();
+	let session = cookie_header
+		.substring_after("NOY_SESSION=")
+		.unwrap()
+		.substring_before(";")
+		.unwrap();
+
+	defaults_set("session", StringRef::from(session).0);
+
+	Ok(session.to_string())
+}
+
+pub fn get_json(url: String, body: String) -> Result<ValueRef, AidokuError> {
+	let request = gen_request(url, HttpMethod::Post).body(body.as_bytes());
+
+	request.send();
+
+	if request.status_code() == 401 {
+		request
+			.header("Cookie", &format!("NOY_SESSION={}", &login()?))
+			.json()
+	} else {
+		request.json()
+	}
 }
 
 pub fn gen_cover_url(manga_id: String) -> String {
